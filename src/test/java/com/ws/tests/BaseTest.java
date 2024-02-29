4bf537936a507539b37a9069bcc4d54bff6830ab
DURJOY <DURJOY@DESKTOP-7LUH7AU.mynetworksettings.com>
2024-02-29 11:46:50 -0500

    added a base test for reuseability

---
+++ src/test/java/com/ws/tests/BaseTest.java
@@ -0,0 +1,29 @@
+package com.ws.tests;
+
+import org.testng.annotations.BeforeMethod;
+
+import com.ws.base.DriverScript;
+import com.ws.pages.HomePage;
+import com.ws.pages.LogInPage;
+
+public class BaseTest extends DriverScript {
+	
+	HomePage homepage;
+	LogInPage loginpage;
+	
+	@BeforeMethod
+	public void setup()
+	{
+		initApplication();
+		homepage = new HomePage();
+		loginpage = new LogInPage();
+	}
+	
+	public void teardown() 
+	{
+		driver.quit();
+		
+	}
+	
+
+}
