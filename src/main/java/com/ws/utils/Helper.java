2cba4b0ef9ed8713c839b1d079dfd3845064d523
DURJOY <DURJOY@DESKTOP-7LUH7AU.mynetworksettings.com>
2024-02-29 17:26:51 -0500

    added util helper file and updated base test

--- src/main/java/com/ws/utils/Helper.java
+++ src/main/java/com/ws/utils/Helper.java
@@ -1,5 +1,28 @@
 package com.ws.utils;
 
-public class Helper {
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import com.ws.base.DriverScript;
+
+public class Helper extends DriverScript {
+	
+	public static void captureScreen(WebDriver driver) 
+	{
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String screenPath = System.getProperty("user-dir")+ "./reports/screenshots/screen.png";
+		try {
+			FileHandler.copy(src,new File(screenPath));
+		} 
+		  catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+	}
 
 }
--- src/test/java/com/ws/tests/BaseTest.java
+++ src/test/java/com/ws/tests/BaseTest.java
@@ -2,27 +2,49 @@ package com.ws.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.xml.SuiteGenerator;
 
+import com.aventstack.extentreports.ExtentReporter;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.ws.base.DriverScript;
 import com.ws.pages.HomePage;
 import com.ws.pages.LogInPage;
 
 public class BaseTest extends DriverScript {
 	
+	protected static ExtentReporter extent;
+	 protected static ExtentReports report;
+	 protected static ExtentTest logger;
+	
+	
 	HomePage homepage;
 	LogInPage loginpage;
+	
+	@BeforeSuite
+	public void setUpReport()
+	{
+	extent = new ExtentHtmlReporter("./reports/index.html");
+	report = new ExtentReports();
+	report.attachReporter(extent);
+	}
 	
 	@BeforeMethod
 	public void setup()
 	{
+		
 		initApplication();
 		homepage = new HomePage();
 		loginpage = new LogInPage();
+		
 	}
 	
 	@AfterMethod
 	public void teardown() 
 	{
+		report.flush();
 		driver.quit();
 		
 	}
