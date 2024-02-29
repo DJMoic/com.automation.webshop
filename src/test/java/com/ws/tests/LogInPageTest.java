b9cb432c76c5d9fa1e23388e7cb437554682cba4
DURJOY <DURJOY@DESKTOP-7LUH7AU.mynetworksettings.com>
2024-02-29 11:36:43 -0500

    change in test case

--- src/test/java/com/ws/tests/LogInPageTest.java
+++ src/test/java/com/ws/tests/LogInPageTest.java
@@ -11,20 +11,32 @@ import com.ws.pages.LogInPage;
 
 public class LogInPageTest extends DriverScript{
 	
-	@Test
-	public void testHomePageTitle() {
+	HomePage homepage;
+	LogInPage loginpage;
+	
+	
+	public void setup()
+	{
 		initApplication();
-		HomePage homepage = new HomePage();
+		homepage = new HomePage();
+		loginpage = new LogInPage();
+	}
+	
+	public void teardown() 
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void testHomePageTitle() 
+	{
 		String title = homepage.getHomePageTitle();
 		Assert.assertTrue(title.contains("Demo Web Shop"));
-		quitDriver();
+		
 	}
 	@Test
 	public void testLoginFucntion() {
-		initApplication();
-		HomePage homepage = new HomePage();
 		homepage.clickLogin();
-		LogInPage loginpage = new LogInPage();
 	    String loginpagetitle = loginpage.getLoginPageTitle();
 	    Assert.assertTrue(loginpagetitle.contains("Login"));
 	    loginpage.enterEmail("seleautouser01@test.com");
@@ -33,17 +45,14 @@ public class LogInPageTest extends DriverScript{
 		String title = homepage.getHomePageTitle();
 		Assert.assertTrue(title.contains("Demo Web Shop"));
 		homepage.clickLogout();
-		quitDriver();
+		
 				
 	}
 	@Test
 	public void testForgotPasswordLink() {
-		initApplication();
-		HomePage homepage = new HomePage();
 		homepage.clickLogin();
-		LogInPage loginpage = new LogInPage();
 		loginpage.isForgotpasswordLinkPresent();
-		quitDriver();
+		
 	   
 		
 	}
