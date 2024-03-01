da0c88c45cbcc0e224c0a06a8cad43e19adc33e1
DURJOY <DURJOY@192.168.1.158>
2024-03-01 15:15:28 -0500

    made changes to the files

--- src/main/java/com/ws/utils/ExcelUtils.java
+++ src/main/java/com/ws/utils/ExcelUtils.java
@@ -1,5 +1,40 @@
 package com.ws.utils;
 
-public class Excelreader {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+	XSSFWorkbook wb;
+	
+	/*
+	 * This constructor is used to read excel file
+	 */
+
+	public ExcelUtils(String excelPath) {
+		try {
+			File file = new File(excelPath);
+			FileInputStream fis = new FileInputStream(file);
+		     wb = new XSSFWorkbook(fis);
+		} 
+		    catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+		public int getRowCount(int sheetNo) {
+		int count = wb.getSheetAt(sheetNo).getLastRowNum()+1;
+		return count;
+		}
+		
+		public String getcellData(int sheetNo,int row, int cell ) {
+			String data = wb.getSheetAt(sheetNo).getRow(row).getCell(cell).toString();
+			return data;
+		
+			
+		
+	}
 
 }
